a2c97d87cb51a5f148227547e5745f1ebc9e8410
dhtkevo <110263079+Dhtkevo@users.noreply.github.com>
2025-03-23 12:07:06 -0400

    Display title of post when leaving a new comment

--- components/CommentForm/CommentForm.tsx
+++ components/CommentForm/CommentForm.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
+interface Post {
+  id: number;
+  title: string;
+  content: string;
+  createdAt: string;
+}
+
 export default function CommentForm() {
-  const params = useParams();
+  const { postId } = useParams();
+  const [post, setPost] = useState<Post | null>(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/posts/" + postId)
+      .then((response) => response.json())
+      .then((response) => setPost(response));
+  }, []);
 
   return (
     <div className="bg-gray-200 h-screen flex flex-col items-center justify-center">
-      <h1 className="text-5xl font-bold mb-64">Replying to: </h1>
+      <h1 className="text-5xl font-bold mb-64">
+        Replying to post: {post && post.title}
+      </h1>
       <form method="POST" action="http://localhost:3000/">
         <div className="flex flex-col gap-4 shadow-2xl border-gray-400 border p-6 rounded-2xl">
           <label htmlFor="commentContent">Comment Content: </label>
