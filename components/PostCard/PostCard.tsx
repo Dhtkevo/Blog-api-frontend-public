68588b5a0299f67a7d30ae3c971a15c20a6e384f
dhtkevo <110263079+Dhtkevo@users.noreply.github.com>
2025-03-22 22:04:06 -0400

    Populate post card with post information

--- components/PostCard/PostCard.tsx
+++ components/PostCard/PostCard.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 
-export default function PostCard() {
+interface postCardProps {
+  title: string;
+  content: string;
+  createdAt: string;
+}
+
+export default function PostCard({ title, content, createdAt }: postCardProps) {
   return (
     <div className="flex flex-col gap-6 bg-amber-50 max-w-1/3 rounded-3xl px-4 py-2 shadow-md">
-      <h2 className="self-center font-bold">Post Title Goes Here</h2>
-      <p>Post content Goes here</p>
-      <p className="self-center">Time Stamp of Post Goes Here</p>
+      <h2 className="self-center font-bold">{title}</h2>
+      <p>{content}</p>
+      <p className="self-center">{createdAt}</p>
       <button className="rounded-3xl border-2 hover:cursor-pointer hover:bg-amber-100">
         Leave Comment
       </button>
--- components/PostsContainer/PostsContainer.tsx
+++ components/PostsContainer/PostsContainer.tsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PostCard from "../PostCard/PostCard";
 
+interface Posts {
+  id: number;
+  title: string;
+  content: string;
+  createdAt: string;
+}
+
 export default function PostsContainer() {
+  const [posts, setPosts] = useState<Posts[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/posts")
+      .then((response) => response.json())
+      .then((response) => setPosts(response));
+  }, []);
+
+  const postItems = posts.map((post) => (
+    <PostCard
+      key={post.id}
+      title={post.title}
+      content={post.content}
+      createdAt={post.createdAt}
+    />
+  ));
+
   return (
     <div className="flex flex-col gap-4 border-2 border-red-600 h-screen px-14 py-6 bg-gray-200">
       <h1 className="text-4xl font-bold self-center">Kevin's Blog Posts</h1>
-      <div>
-        <PostCard />
-      </div>
+      <div>{postItems}</div>
     </div>
   );
 }
